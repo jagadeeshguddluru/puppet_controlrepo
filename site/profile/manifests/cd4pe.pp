573ae2db336c2b0c4cc5da4f8ccc7186308a11c2
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2020-01-13 15:20:50 +0000

    Initial test of cd4pe

--- site/profile/manifests/cd4pe.pp
+++ site/profile/manifests/cd4pe.pp
@@ -1,5 +1,6 @@
 class profile::cd4pe (
-  String $cd4pe_version = 'latest',
+  Enum['source', 'replica'] $ha_mode = 'source',
+  String $cd4pe_version              = 'latest',
 ) {
   class { '::cd4pe':
     cd4pe_version        => $cd4pe_version,
@@ -7,5 +8,29 @@ class profile::cd4pe (
     agent_service_port   => 7000,
     backend_service_port => 8000,
     web_ui_port          => 8080,
+  }
+
+  case $ha_mode {
+    'source': {
+      $service_ensure     = 'running'
+      $service_enable     = true
+      $docker_run_running = true
+    }
+    'replica': {
+      $service_ensure     = 'stopped'
+      $service_enable     = false
+      $docker_run_running = false
+    }
+  }
+
+  # Set the state of the service manually. This means that the replica will not
+  # be running, but will be ready to go
+  Docker::Run <| title == 'cd4pe' |> {
+    running => $docker_run_running,
+  }
+
+  Service <| name == 'pe-postgresql' |> {
+    ensure  => $service_ensure,
+    enable  => $service_enable,
   }
 }
